b3141a3ce38e8b1c837859962d66d6bf6e60739b
kamyllymcp <137413690+kamyllymcp@users.noreply.github.com>
2023-10-19 10:11:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-function tocaSom (idElememtoAudio) {
+function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
-for (let contador = 0; contador <listaDeTeclas.length; contador++) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1]
-    const idAudio = `#som_${instrumentp}`; //template string
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    tecla.onclick = fuction () {
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    
+
 }
